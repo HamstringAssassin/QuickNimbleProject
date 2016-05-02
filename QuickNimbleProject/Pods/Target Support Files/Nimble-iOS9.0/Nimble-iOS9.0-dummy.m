1b03ae5bb1b717ea8ed46bcf514305261f1e9d9d
Alan O'Connor <alanoconnor@Alans-MacBook-Pro.local>
2016-05-02 23:40:31 +0100

    updated project with cocoapods. added quick / nimble to project.

---
+++ QuickNimbleProject/Pods/Target Support Files/Nimble-iOS9.0/Nimble-iOS9.0-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Nimble_iOS9_0 : NSObject
+@end
+@implementation PodsDummy_Nimble_iOS9_0
+@end
---
+++ QuickNimbleProject/Pods/Target Support Files/Pods-QuickNimbleProject/Pods-QuickNimbleProject-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_QuickNimbleProjectVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_QuickNimbleProjectVersionString[];
+
